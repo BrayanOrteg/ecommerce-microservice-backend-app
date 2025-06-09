80aa7491f8ef8e9f20e0db7a4beca8c3fb4e4ab1
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2025-06-09 01:30:44 -0500

    feat: JaCoCo coverage tests

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -10,32 +10,32 @@ class EcommerceLoadTest(HttpUser):
 
     @task
     def get_users(self):
-        self.client.get("user-service/api/users")
+        self.client.get("/user-service/api/users")
 
     @task
     def get_products(self):
-        self.client.get("product-service/api/products")
+        self.client.get("/product-service/api/products")
 
     @task
     def get_categories(self):
-        self.client.get("product-service/api/categories")
+        self.client.get("/product-service/api/categories")
 
     @task
     def get_orders(self):
-        self.client.get("order-service/api/orders")
+        self.client.get("/order-service/api/orders")
 
     @task
     def get_credentials(self):
-        self.client.get("user-service/api/credentials")
+        self.client.get("/user-service/api/credentials")
 
     @task
     def get_payments(self):
-        self.client.get("payment-service/api/payments")
+        self.client.get("/payment-service/api/payments")
 
     @task
     def get_shippings(self):
-        self.client.get("shipping-service/api/shippings")
+        self.client.get("/shipping-service/api/shippings")
 
     @task
     def get_favourites(self):
-        self.client.get("favourite-service/api/favourites")
+        self.client.get("/favourite-service/api/favourites")
